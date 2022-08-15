5c2b7cb538ad52815f73a5fa8ef2626bb46b2150
Sarthak3232 <111259517+Sarthak3232@users.noreply.github.com>
2022-08-14 19:27:03 -0700

    testing trajectory planner

--- TrajectoryPlanner/src/main/java/com/example/trajectoryplanner/TrajectoryPlanner.java
+++ TrajectoryPlanner/src/main/java/com/example/trajectoryplanner/TrajectoryPlanner.java
@@ -11,17 +11,13 @@ public class TrajectoryPlanner {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
                                 .forward(30)
+                                .lineToLinearHeading(new Pose2d(50,50, Math.toRadians(180)))
                                 .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                                .forward(10)
                                 .build()
                 );
 
